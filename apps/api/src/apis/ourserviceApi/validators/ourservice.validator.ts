c6595e2672145db3f1fe75e43ae69c0e6bfc3790
NaVeeN-PANCHAL <iamnaveen1212@gmail.com>
2025-11-18 13:18:25 +0530

    ../cahnge

--- apps/api/src/apis/ourserviceApi/validators/ourservice.validator.ts
+++
@@ -1,8 +0,0 @@
-// src/apis/ourserviceApi/validators/ourservice.validator.ts
-import { body } from "express-validator";
-
-export const createOurServiceValidator = [
-  body("serviceName").notEmpty().withMessage("Service name is required"),
-  body("price").isNumeric().withMessage("Price must be a number"),
-  body("title").notEmpty().withMessage("Title is required"),
-];
